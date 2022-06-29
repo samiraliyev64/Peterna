8a36b76f82b6db376dc74864071fb39c2a3e1f3c
Emjliy <6p304s8@code.edu.az>
2022-06-29 17:54:06 +0400

    add backend

---
+++ backend/backend/Areas/AdminPanel/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Areas.AdminPanel.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [Area("AdminPanel")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
